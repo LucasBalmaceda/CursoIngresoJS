9f17f204675d9519f60f410bce6fa8417b297009
LucasBalmaceda <52472495+LucasBalmaceda@users.noreply.github.com>
2019-07-03 22:24:04 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,17 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var sueldo
+    var descuento
+    var resultado 
+    
+    sueldo = document.getElementById("importe").value;
+    sueldo = parseFloat(sueldo);
+    
+    descuento = sueldo * .25;
+    
+    resultado = sueldo - descuento;
+    document.getElementById("resultado").value = resultado;
+
+    
 }
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,47 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+ var num1 = document.getElementById("numeroUno").value;
+ num1 = parseInt(num1);
+ var num2 = document.getElementById("numeroDos").value;
+ num2 = parseInt(num2);
+ var suma = num1 + num2;
+
+ alert("Su suma es " + suma);
+
+
 }
 
 function restar()
 {
-	
+var num1 = document.getElementById("numeroUno").value;
+ num1 = parseInt(num1);
+ var num2 = document.getElementById("numeroDos").value;
+ num2 = parseInt(num2);
+ var resta = num1 - num2;
+
+ alert("Su resta es " + resta);	
 }
 
 function multiplicar()
 { 
-	
+var num1 = document.getElementById("numeroUno").value;
+ num1 = parseInt(num1);
+ var num2 = document.getElementById("numeroDos").value;
+ num2 = parseInt(num2);
+ var multiplicacion = num1 * num2;
+
+ alert("Su multiplicación es " + multiplicacion);		
 }
 
 function dividir()
 {
-	
+var num1 = document.getElementById("numeroUno").value;
+ num1 = parseInt(num1);
+ var num2 = document.getElementById("numeroDos").value;
+ num2 = parseInt(num2);
+ var division = num1 / num2;
+
+ alert("Su multiplicación es " + division);	
 }
 
